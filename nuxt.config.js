585b29c55180a54c2d7dc1609f3aaf8614ee6673
Nicolás Montenegro <niko_nmv@live.cl>
2020-01-19 18:10:29 -0300

    Update modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,7 @@ export default {
   /*
   ** Nuxt.js dev-modules
   */
-  devModules: [
+  buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module'
   ],
