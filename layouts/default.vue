9f8628a270dd1ee31321be157afa2eadcaf4bf9f
Nicolás Montenegro <niko_nmv@live.cl>
2021-07-06 00:40:40 -0400

    Menu sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,7 @@
 <template lang="pug">
-.container.px-2(:class='containerClass')
-  .columns.is-desktop.is-gapless.is-justify-content-space-between
-    //- .column.is-3-desktop
-    //-   SectionBar
+#template.container.px-2(:class='containerClass')
+  Menu.container-menu
+  .container-content.columns.is-desktop.is-gapless.is-justify-content-space-between
     .column.is-12.content.main
       template(v-if='navegation !== null')
         h1.title.is-1.mb-1.mt-4(v-if='navegation.section.name')
@@ -11,14 +10,15 @@
           nuxt-link(:to='localePath({name: navegation.back.name})')
             strong {{ $t('navigation.back_to', { section: $t(`sections.${navegation.back.name}`) }) }} 
       nuxt
-  
-  Footer.mt-6.mb-4
+    .column.is-12.content.main
+      Footer.mt-6.mb-4
 </template>
 
 <script>
 export default {
   components: {
-    SectionBar: () => import('~/components/SectionBar.vue'),
+    // SectionBar: () => import('~/components/SectionBar.vue'),
+    Menu: () => import('~/components/Menu.vue'),
     Footer: () => import('~/components/Footer.vue')
   },
   computed: {
